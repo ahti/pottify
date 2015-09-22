cd99b8a2a67672a6d58ca357be0fa5481293d7ef
Lukas Stabe <lukas@stabe.de>
2015-09-22 18:45:39 +0200

    fix counter :party:

--- assets/js/counter.rb
+++ assets/js/counter.rb
@@ -5,12 +5,12 @@ module Pottify
     define_state(:count) { 0 }
 
     def handle_click
-      set_state count: count + 1
+      self.count += 1
     end
 
     def render
       div do
-        button { "#{self.count}" }.on(:click) { handle_click }
+        h1 { "#{self.count}" }.on(:click) { handle_click }
         present Spinner
       end
     end
