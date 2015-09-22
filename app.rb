9f9e715d42a3365c5c9656d65fc89c4c433fa56f
Hans Ole Hatzel <hhatzel@gmail.com>
2015-09-22 18:04:16 +0200

    Opal-browser example added.

--- app.rb
+++ app.rb
@@ -1,7 +1,12 @@
 require 'sinatra/base'
+require 'sinatra/json'
 
 class PottifyApp < Sinatra::Base
   get '/' do
     erb :index, format: :html5
+  end
+
+  get '/feeds' do
+    json [{name: "Thsi feed", id: 20}, {name: "My special feed", id: 21}]
   end
 end
--- assets/js/app.rb
+++ assets/js/app.rb
@@ -1,9 +1,17 @@
 require 'opal'
-require 'opal-browser'
+require 'browser'
+require 'browser/http'
 require 'react'
 require 'js/counter'
 
 # Workaround for opal bug
 require 'react/ext/string'
 
+Browser::HTTP.get "/feeds" do
+  on :success do |res|
+    alert res.json.inspect
+  end
+end
+
 React.render(React.create_element(Pottify::Counter), `document.getElementById('root')`)
+
