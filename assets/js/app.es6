a6e2b323ffed75eda6544650fd543716bada867c
Hans Ole Hatzel <hhatzel@gmail.com>
2015-09-22 03:33:06 +0200

    Interesting counter.

--- assets/js/app.es6
+++ assets/js/app.es6
@@ -1,6 +1,12 @@
 //= require js/components/hello.js
+//= require js/components/counter.js
 
 React.render(
   <Hello name="John" />,
   document.getElementById('root')
 );
+
+// React.render(
+//   <Counter/>,
+//   document.getElementById('root')
+// );
---
+++ assets/js/components/counter.js
@@ -0,0 +1,13 @@
+class Counter extends React.Component {
+  constructor() {
+    this.count = 0
+  }
+
+  // handleClick = () => {
+  //   this.setState({count: this.count + 1})  
+  // }
+
+  // render = () => {
+  //   return <h1 onClick={this.handleClick}>{this.count}</h1>
+  // }
+}
